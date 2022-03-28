80c57db34e43b4e9382cda15fe6ecb4a8a2f3dee
Jayant Solanki <jayantjnp@gmail.com>
2022-03-27 22:22:21 -0700

    'staging'

--- epi_judge_python/5-18-spiral_ordering.py
+++ epi_judge_python/5-18-spiral_ordering.py
@@ -208,6 +208,10 @@ matrix_in_spiral_order_mn([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
 matrix_in_spiral_order_mn([[1,2,3],[4,5,6],[7,8,9]])
 matrix_in_spiral_order_mn([[1,2,3,4,5],[6, 7,8,9,10],[11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]])
 
+#variant 5
+"""
+Compute the last element in the spiral order for an mxn 2d array A in O(1) time
+"""
 
 
 
